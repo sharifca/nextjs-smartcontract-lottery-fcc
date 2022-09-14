8b802188baa05f09bcfb23ed1f9d95c2ba5bbebe
sharifca <chichang@MASTERDELL.localdomain>
2022-09-14 14:25:39 +0700

    added dot

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -105,6 +105,7 @@ export default function LotteryEntrance() {
                     <div> Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
                     <div> Number Of Players: {numPlayers} </div>
                     <div> Recent Winner: {recentWinner} </div>
+                    <div>WHATS UPP</div>
                 </div>
             ) : (
                 <div>No Raffle Address Detected!</div>
